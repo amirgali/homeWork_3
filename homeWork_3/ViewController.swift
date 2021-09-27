14d8c1167f8ecafe7bcc0d2933237c36819a2392
Amirgali <aturalinov@yandex.ru>
2021-09-27 18:27:54 +0300

    a

---
+++ homeWork_3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homeWork_3
+//
+//  Created by Амиргали Туралинов on 27.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
